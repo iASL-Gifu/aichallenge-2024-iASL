ea0c2260ea8841ea197ae2c762b74875d3204aa1
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2024-08-31 17:58:30 +0900

    edit object_info "add param"

--- aichallenge/workspace/src/aichallenge_submit/original_pkg/object_info_publisher/src/object_info_publisher_node.cpp
+++ aichallenge/workspace/src/aichallenge_submit/original_pkg/object_info_publisher/src/object_info_publisher_node.cpp
@@ -12,6 +12,9 @@ class MergeAndPublishNode : public rclcpp::Node
 public:
   MergeAndPublishNode() : Node("merge_and_publish_node")
   {
+
+    this->declare_parameter<double>("scale", 2.0);
+    this->get_parameter("scale", scale_);
     // Subscribe to the PredictedObjects topic
     objects_subscription_ = this->create_subscription<autoware_auto_perception_msgs::msg::PredictedObjects>(
       "/perception/object_recognition/objects", 10,
@@ -89,9 +92,9 @@ private:
 
         // Add additional details like shape or classification as needed
         object.shape.type = autoware_auto_perception_msgs::msg::Shape::CYLINDER;
-        object.shape.dimensions.x = last_data_msg_->data[i + 3] * 2; // Example: diameter -> dimensions
-        object.shape.dimensions.y = last_data_msg_->data[i + 3] * 2;
-        object.shape.dimensions.z = last_data_msg_->data[i + 3] * 2;
+        object.shape.dimensions.x = last_data_msg_->data[i + 3] * scale_; // Example: diameter -> dimensions
+        object.shape.dimensions.y = last_data_msg_->data[i + 3] * scale_;
+        object.shape.dimensions.z = last_data_msg_->data[i + 3] * scale_;
 
         autoware_auto_perception_msgs::msg::ObjectClassification classification;
         classification.label = autoware_auto_perception_msgs::msg::ObjectClassification::UNKNOWN;
@@ -124,6 +127,8 @@ private:
   // Store the last received messages
   autoware_auto_perception_msgs::msg::PredictedObjects::SharedPtr last_objects_msg_;
   std_msgs::msg::Float64MultiArray::SharedPtr last_data_msg_;
+
+  double scale_ = 2;
 };
 
 int main(int argc, char *argv[])
